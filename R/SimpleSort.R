399a356b62844219d67ef7380fed2d26e846a092
Johannes Graumann <jog2030@qatar-med.cornell.edu>
2012-11-20 12:59:44 +0300

    Exploring first analysis paths.

---
+++ R/SimpleSort.R
@@ -0,0 +1,24 @@
+#' @export
+SimpleSort <- function(){
+  # Discard reverse/contaminants
+  saveData <- pGroups
+  saveData <- saveData[saveData$Reverse != "+",]
+  saveData <- saveData[saveData$Contaminant != "+",]
+  rownames(saveData) <- NULL
+  # Extract columns containing NOT-NORMALIZED ratios and assemble
+  tmpData <- saveData[grep(pattern="^Ratio H/L Exp. [[:alpha:]]{1}",names(saveData))]
+  ratioMin <- sapply(split(tmpData,seq(nrow(tmpData))),which.min)
+  simpleData <- saveData[ratioMin == 4,]
+  simpleData <- simpleData[order(simpleData["Ratio H/L Exp. D"]),]
+  topX <- 50
+  write.table(
+    x = simpleData,
+    file = file.path(
+      "/tmp",
+      paste(
+        Sys.Date(),
+        "MinRatioInFracD.txt",
+        sep="")
+    ),
+    sep = "\t",row.names=FALSE,col.names=TRUE)
+}
