a6f4fce33c79881601511e2c40229ea8ec58a112
Johannes Graumann <jog2030@qatar-med.cornell.edu>
2012-11-25 12:17:54 +0300

    Add a function destined to document the analysis flow for the data set.

---
+++ R/CompleteAnalysis.R
@@ -0,0 +1,56 @@
+#' @export
+CmpleteAnalysis <- function(){}
+#   # Where do the ratio minima reside 1?
+#   #ratioMin <- sapply(split(tmpData,seq(nrow(tmpData))),which.min)
+#   # Impute missing values
+#   #library(pcaMethods)
+#   #imputationMethod <- "bpca"
+#   #scaleMethod <- "none"
+#   #centering <- FALSE
+#   #pcares <- pca(
+#   #  object=t(tmpData),
+#   #  method=imputationMethod,
+#   #  scale=scaleMethod,
+#   #  center=centering)
+#   #tmpData <- as.data.frame(t(slot(pcares,"completeObs")))
+#   #rm(pcares)
+#   # Where do the ratio minima reside 2?
+#   #imputedRatioMin <- sapply(split(tmpData,seq(nrow(tmpData))),which.min)
+#   # 
+#   # Correlate
+#   correlationMethod <- "spearman"
+#   cCoeffs <- sapply(
+#     seq(nrow(tmpData)),
+#     function(x){
+#       pcp <- unlist(tmpData[x,])
+#       cCoeff <- cor(
+#         pcp,
+#         flippaseActivity[["Av. Spec. Activity"]] * -1,
+#         method=correlationMethod)
+#       return(cCoeff)
+#     })
+#   # Assembly
+#   names(tmpData) <- paste("Imputed",names(tmpData))
+#   saveData$RatioMin <- ratioMin
+#   saveData <- cbind(saveData,tmpData,stringsAsFactors=FALSE)
+#   saveData$cCoeffs <- cCoeffs
+#   saveData$ImputedRatioMin <- imputedRatioMin
+#   # Order by cCoeff
+#   saveData <- saveData[order(saveData$cCoeffs,decreasing=TRUE),]
+#   # First x candidates
+#   topX <- 50
+#   write.table(
+#     x = saveData[seq(topX),],
+#     file = file.path(
+#       "/tmp",
+#       paste(
+#         Sys.Date(),
+#         "_Top",topX,
+#         "_Cor-",correlationMethod,
+#         "_Imput-",imputationMethod,
+#         "_Scale-",scaleMethod,
+#         "_Center-",centering,
+#         ".txt",
+#         sep="")
+#     ),
+#     sep = "\t",row.names=FALSE,col.names=TRUE)
