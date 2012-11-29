3fd040c914d82d76093815d87d8b31d517e1f94b
Johannes Graumann <jog2030@qatar-med.cornell.edu>
2012-11-29 17:37:59 -0500

    Make the function return data with maximum ratio in arbitrary fractions (as defined by argument "Fraction").

--- R/SimpleSort.R
+++ R/SimpleSort.R
@@ -1,5 +1,11 @@
 #' @export
-SimpleSort <- function(){
+SimpleSort <- function(Fraction=NA){
+  if(!is.vector(Fraction,mode="numeric")){
+    stop("\"Fraction\" must be numeric.")
+  }
+  if(length(Fraction) != 1){
+    stop("\"Fraction\" must be of length 1.")
+  }
   # Discard reverse/contaminants
   saveData <- pGroups
   saveData <- saveData[saveData$Reverse != "+",]
@@ -14,7 +20,10 @@ SimpleSort <- function(){
   ratioMax <- sapply(split(tmpData,seq(nrow(tmpData))),which.max)
   # Assemble and filter
   simpleData <- cbind(saveData,tmpData)
-  simpleData <- simpleData[ratioMax == 4,]
-  simpleData <- simpleData[order(simpleData["Ratio L/H Exp. D"],decreasing=TRUE),]
+  simpleData <- simpleData[ratioMax == Fraction,]
+  simpleData <- simpleData[
+    order(
+      simpleData[grep(pattern="^Ratio H/L Exp. [[:alpha:]]{1}",names(simpleData))[Fraction]],
+      decreasing=TRUE),]
   return(simpleData)
 }
