76192c1b8d0a23b27195615e6f0f440f33a2a90c
Johannes Graumann <jog2030@qatar-med.cornell.edu>
2013-02-11 11:08:00 +0300

    Add a new request by AM;

--- R/CompleteAnalysis.R
+++ R/CompleteAnalysis.R
@@ -97,6 +97,7 @@ CompleteAnalysis <- function(directory=tempdir()){
       "IFA38","HXT3","RTN1","ERG3","YNR021W","PGA3","ERP2","SEC61","ERG5",
       "TSC13","VMA13","GSF2","TPO4","LCB1","ERP1","SNA2","STE24","FAT1","SEC62",
       "WBP1","YHL017W","YNL181W","DPL1","VOA1","OST1","SOP4","ARV1"))
+  FilteredProfilePlots(fig1Data,c("STE24"))
   # Cooperativity Erg1/11?
   Figure6 <- AdditiveFilteredProfilePlots(fig1Data,c("Erg1","Erg11"))
 }
