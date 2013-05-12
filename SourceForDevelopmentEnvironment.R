cc08e185b95461b11fa0a7f204ed14eae33e9ab5
Johannes Graumann <jog2030@qatar-med.cornell.edu>
2013-05-12 22:25:21 +0300

    Ensure that RCFPD is loaded from the development environment and NOT globally;

--- SourceForDevelopmentEnvironment.R
+++ SourceForDevelopmentEnvironment.R
@@ -3,5 +3,7 @@ library(devtools)
 library(testthat)
 # Switch to developer's mode
 dev_mode(path="~/R-DevTools/mdaa-Development")
+# Ensure correct RCFPD package
+library(RCFPD,lib.loc="~/R-DevTools/mdaa-Development")
 # Load the worked on package
 library(MDAA)
