2d36603336991b60dc7cb281c3c3958aef72550e
Johannes Graumann <jog2030@qatar-med.cornell.edu>
2013-08-29 17:18:45 +0300

    Add a funtion to determine the shortest spectrum in a set;

---
+++ R/MinimumAcquisitionTime.R
@@ -0,0 +1,32 @@
+MinimumAcquisitionTime <- function(x=NA){
+  #######################
+  # Check prerequisites #
+  #######################
+  if(!is.character(x)){
+    stop("'x' must be of class 'character'.")
+  }
+  if(!all(file.exists(x))){
+    stop("All elements in 'x' must refer to existing files.")
+  }
+  if(any(file.access(x,mode=4) == -1)){
+    stop("All elements in 'x' must refer to readable files.")
+  }
+  ##############
+  # Processing #
+  ##############
+  # Parsing
+  #########
+  tmpData <- lapply(
+    x,
+    function(y){ParseFluorometerData(SpecFile=x)})
+  # Extract data
+  ##############
+  # What is the latest data point per series?
+  maxAT <- vapply(tmpData,function(y){y$"Maximal Acquisition Time (s)"},1)
+  # Identify the shortest one
+  minMaxAT <- min(maxAT,na.rm=TRUE)
+  ##########
+  # Return #
+  ##########
+  return(minMaxAT)
+}
