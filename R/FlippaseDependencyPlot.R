31d23581bb4db9634b8864b43c9906f99c7d443a
Johannes Graumann <jog2030@qatar-med.cornell.edu>
2013-08-29 17:17:15 +0300

    Switch to "vapply" for type safe return;

--- R/FlippaseDependencyPlot.R
+++ R/FlippaseDependencyPlot.R
@@ -23,28 +23,28 @@ FlippaseDependencyPlot <- function(x=NA,ReactionVolumes=c(2000,2040),ePC=4.5){
   if(is.null(names(x))){
     stop("All elements on level 1 of 'x' must be named.")
   }
-  if(!all(sapply(x,is.list))){
+  if(!all(vapply(x,is.list,TRUE))){
     stop("All elements in 'x' must be of class 'list'.")
   }
   if(listDepth(x) != 2){
     stop("'x' must be of depth 2.")
   }
-  if(any(sapply(x,function(y){is.null(names(y))}))){
+  if(any(vapply(x,function(y){is.null(names(y))},TRUE))){
     stop("All elements on level 2 of 'x' must be named.")
   }
   nesLev2Names <- "Path"
   facLev2Names <- c("ReactionVolumes","ePC")
   legLev2Names <- c(nesLev2Names,facLev2Names)
-  if(!all(sapply(x,function(y){identical(intersect(names(y),legLev2Names),names(y))}))){
+  if(!all(vapply(x,function(y){identical(intersect(names(y),legLev2Names),names(y))},TRUE))){
     stop("All names on level 2 of 'x' must be from '",paste(legLev2Names,collapse="','"),"'.")
   }
-  if(!any(sapply(x,function(y){identical(intersect(names(y),nesLev2Names),names(y))}))){
+  if(!any(vapply(x,function(y){identical(intersect(names(y),nesLev2Names),names(y))},TRUE))){
     stop("Elements with names '",paste(nesLev2Names,collapse="','"),"' are compulsory on level 2 of 'x'.")
   }
-  if(!all(sapply(x,function(y){is.character(y$Path) & length(y$Path) ==1}))){
+  if(!all(vapply(x,function(y){is.character(y$Path) & length(y$Path) ==1},TRUE))){
     stop("All 'Path' elements in 'x' must be single length 'character' objects.")
   }
-  if(!all(sapply(
+  if(!all(vapply(
     x,
     function(y){
       output <- TRUE
@@ -54,10 +54,14 @@ FlippaseDependencyPlot <- function(x=NA,ReactionVolumes=c(2000,2040),ePC=4.5){
         }
       }
       return(output)
-    }))){
+    },
+    TRUE))){
     stop("All 'ReactionVolumes' elements in 'x' must be 'numeric' objects of length 2.")
   }
-  if(!all(sapply(
+  if(!is.numeric(ePC) | length(ePC) != 1){
+    stop("'ePC' must be 'numeric' objects of length 1.")
+  }
+  if(!all(vapply(
     x,
     function(y){
       output <- TRUE
@@ -67,13 +71,14 @@ FlippaseDependencyPlot <- function(x=NA,ReactionVolumes=c(2000,2040),ePC=4.5){
         }
       }
       return(output)
-    }))){
+    },
+    TRUE))){
     stop("All 'ePC' elements in 'x' must be 'numeric' objects of length 1.")
   }
-  if(!all(file.exists(sapply(x,function(x){x$Path})))){
+  if(!all(file.exists(vapply(x,function(y){y$Path},"A")))){
     stop("All 'Path' elements in 'x' must refer to existing files.")
   }
-  if(any(file.access(sapply(x,function(y){y$Path}),mode=4) == -1)){
+  if(any(file.access(vapply(x,function(y){y$Path},"A"),mode=4) == -1)){
     stop("All 'Path' elements in 'x' must refer to readable files.")
   }
   ##############
@@ -86,32 +91,34 @@ FlippaseDependencyPlot <- function(x=NA,ReactionVolumes=c(2000,2040),ePC=4.5){
     x,
     function(y){ParseFluorometerData(SpecFile=y$Path)})
   # What intensity to extract?
-  minAT <- sapply(tmpData,function(y){y$"Minimal Acquisition Time (s)"})
+  minAT <- vapply(tmpData,function(y){y$"Minimal Acquisition Time (s)"},1)
   if(!all(minAT[1] == minAT)){
     stop("Minimum acquisition times are not identical - aborting.")
   }
   minAT <- unique(minAT)
-  maxAT <- min(sapply(tmpData,function(y){y$"Maximal Acquisition Time (s)"}))
+  tmpAT <- vapply(tmpData,function(y){y$"Maximal Acquisition Time (s)"},1)
   # Calculations
   ##############
   # Average over first 10 values for activity baseline
-  baselineIntensity <- sapply(
+  baselineIntensity <- vapply(
     tmpData,
     function(y){
       tmpFrom <- min(which(y$Data$"Time (s)" >= minAT))
       baselineSS <- seq(from=tmpFrom,to=tmpFrom+9)
       return(median(y$Data$"Fluorescense Intensity"[baselineSS],na.rm=TRUE))
-    })
+    },
+    1)
   # Average over last 10 values (in common time range) for activity
-  activityIntensity <- sapply(
+  activityIntensity <- vapply(
     tmpData,
     function(y){
       tmpTo <- max(which(y$Data$"Time (s)" <= maxAT))
       activitySS <- seq(from=tmpTo-9,to=tmpTo)
       return(median(y$Data$"Fluorescense Intensity"[activitySS],na.rm=TRUE))
-    })
+    },
+    1)
   # Apply volume correction factors as needed
-  tmpRV <- lapply(
+  tmpRV <- vapply(
     tmpData,
     function(y){
       if(is.null(y$"Reaction Volumes")){
@@ -119,8 +126,9 @@ FlippaseDependencyPlot <- function(x=NA,ReactionVolumes=c(2000,2040),ePC=4.5){
       } else {
         return(y$"Reaction Volumes")
       }
-    })
-  tmpCF <- sapply(tmpRV,function(y){y[2]/y[1]})
+    },
+    c(1,2))
+  tmpCF <- vapply(tmpRV,function(y){y[2]/y[1]},1)
   activityIntensity <- activityIntensity * tmpCF
   # Calculate and relativate intensity differences
   deltaIntensity <- 1-(activityIntensity/baselineIntensity)
