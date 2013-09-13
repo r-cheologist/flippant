39743dd43217bdf9622546d3c44dcb313f4afeb8
Johannes Graumann <jog2030@qatar-med.cornell.edu>
2013-09-13 17:34:19 +0300

    Complete remodelling to accept DF rather than list as input; Clarify variable names to something that makes sense;

--- R/FlippaseDependencyPlot.R
+++ R/FlippaseDependencyPlot.R
@@ -1,126 +1,130 @@
-#' @seealso \code{\link{ParseFluorometerData}}
-FlippaseDependencyPlot <- function(
-  x=NA,
-  ReactionVolumes=c(2000,2040),
-  ePC=4.5,
-  ProtC=NA,
-  AcquisitionTimePoint=NA)
+#' @seealso \code{\link{ParseFluorometerData}}, 
+#' \code{\link{ParseFluorometerData2}}, \code{\link{TimepointOfMeasurement}}
+FlippaseDependencySeries <- function(x)
 {
-  x <- list(
-    "15ul"=list(
-      Path="~/localTmp/Fluor Data_Menon Lab/21FEB2013_Erg1 immun_deple/Erg1 TE-minus-15ul.txt",
-      ProtV=15),
-    "40ul"=list(
-      Path="~/localTmp/Fluor Data_Menon Lab/21FEB2013_Erg1 immun_deple/Erg1 TE-minus-40ul.txt",
-      ProtV=40),
-    "75ul"=list(
-      Path="~/localTmp/Fluor Data_Menon Lab/21FEB2013_Erg1 immun_deple/Erg1 TE-minus-75ul.txt",
-      ProtV=75),
-    "150ul"=list(
-      Path="~/localTmp/Fluor Data_Menon Lab/21FEB2013_Erg1 immun_deple/Erg1 TE-minus-150ul.txt",
-      ProtV=150))
-  AcquisitionTimePoint <- MinimumAcquisitionTime(sapply(x,function(y){y$Path}))
-  ReactionVolumes=c(2000,2040)
-  ePC=4.5
-  ProtC=0.67
+  x <- data.frame(
+    Path = c(
+      "~/localTmp/Fluor Data_Menon Lab/21FEB2013_Erg1 immun_deple/Erg1 TE-minus-15ul.txt",
+      "~/localTmp/Fluor Data_Menon Lab/21FEB2013_Erg1 immun_deple/Erg1 TE-minus-40ul.txt",
+      "~/localTmp/Fluor Data_Menon Lab/21FEB2013_Erg1 immun_deple/Erg1 TE-minus-75ul.txt",
+      "~/localTmp/Fluor Data_Menon Lab/21FEB2013_Erg1 immun_deple/Erg1 TE-minus-150ul.txt"),
+    "Extract Volume (ul)" = c(15,40,75,150),
+#     "Reaction Volume Blank (ul)" = rep(2000,4),
+    "Reaction Volume Reconstituted (ul)" = rep(2040,4),
+    "Concentration Egg PC (mM)" = rep(4.5,4),
+    "Extract Protein Concentration (mg/ml)" = rep(0.67,4),
+#     "Timepoint of Measurement (s)",
+    check.names=FALSE,
+    stringsAsFactors=FALSE)
+      
+#   AcquisitionTimePoint <- MinimumAcquisitionTime(sapply(x,function(y){y$Path}))
   #######################
   # Check prerequisites #
   #######################
-  if(is.na(x)[1]){
-    stop("'x' must be defined.")
+  # General DF characteristics
+  ############################
+  if(!is.data.frame(x)){
+    stop("'x' must be of class 'data.frame'.")
   }
-  if(!is.list(x)){
-    stop("'x' must be of class 'list'.")
+  if(nrow(x)==0){
+    stop("'x' must have rows.")
   }
-  if(is.null(names(x))){
-    stop("All elements on level 1 of 'x' must be named.")
+  if(any(is.na(x))){
+    stop("'x' cannot contain 'NA'.")
   }
-  if(!all(vapply(x,is.list,TRUE))){
-    stop("All elements in 'x' must be of class 'list'.")
+  # Required parameters
+  #####################
+  requirements <- list(
+    Name = c(
+      "Path",
+      "Extract Volume (ul)",
+      "Extract Protein Concentration (mg/ml)"),
+    Class = c(
+      "character",
+      "numeric",
+      "numeric"))
+  if(!all( requirements$Name %in% names(x))){
+    stop(
+      "'x' must hold at least the following columns: '",
+      paste(requirements,collapse="', '"),
+      "'.")
   }
-  if(listDepth(x) != 2){
-    stop("'x' must be of depth 2.")
+  if(!identical(
+    unname(vapply(x[requirements$Name],class,c(A="A"))),
+    requirements$Class)){
+    stop(
+      "Required columns '",
+      paste(requirements$Name,collapse="', '"),
+      "' must be of classes '",
+      paste(requirements$Class,collapse="', '"),
+      "'.")
   }
-  if(any(vapply(x,function(y){is.null(names(y))},TRUE))){
-    stop("All elements on level 2 of 'x' must be named.")
+  # Check paths
+  if(!all(file.exists(x$Path))){
+    stop("All entries in column 'Path' must refer to existing files.")
   }
-  nesLev2Names <- c("Path","ProtV")
-  facLev2Names <- c("ReactionVolumes","ePC","ProtC")
-  legLev2Names <- c(nesLev2Names,facLev2Names)
-  if(!all(vapply(x,function(y){identical(intersect(names(y),legLev2Names),names(y))},TRUE))){
-    stop("All names on level 2 of 'x' must be from '",paste(legLev2Names,collapse="','"),"'.")
+  if(any(file.access(x$Path,mode=4) == -1)){
+    stop("All entries in column 'Path' must refer to existing files.")
   }
-  if(!any(vapply(x,function(y){identical(intersect(names(y),nesLev2Names),names(y))},TRUE))){
-    stop("Elements with names '",paste(nesLev2Names,collapse="','"),"' are compulsory on level 2 of 'x'.")
-  }
-  if(!all(vapply(x,function(y){is.character(y$Path) & length(y$Path) ==1},TRUE))){
-    stop("All 'Path' elements in 'x' must be single length 'character' objects.")
-  }
-  if(!all(vapply(
-    x,
-    function(y){
-      output <- TRUE
-      if("ReactionVolumes" %in% names(y)){
-        if(!is.numeric(y$ReactionVolumes) | length(y$ReactionVolumes) != 2){
-          output <- FALSE
-        }
+  # Facultative parameters
+  ########################
+  facultatives <- list(
+    Name = c(
+      "Reaction Volume Blank (ul)",
+      "Reaction Volume Reconstituted (ul)",
+      "Concentration Egg PC (mM)",
+      "Timepoint of Measurement (s)"),
+    Class = c(
+      "numeric",
+      "numeric",
+      "numeric",
+      "numeric"),
+    Default = c(
+      2000,
+      2040,
+      4.5,
+      NA))
+  missing <- which(!(facultatives$Name %in% names(x)))
+  if(length(missing) != 0){
+    for (y in missing){
+      if(facultatives$Name[y] == "Timepoint of Measurement (s)"){
+        warning(
+          "Providing missing column '",
+          facultatives$Name[y],
+          "' from spectra ('Path'). Not appropriate for multiple extract ",
+          "comparisons. You have been warned. See function ",
+          "'TimepointOfMeasurement()'.")
+        addOn <- TimepointOfMeasurement(x$Path)
+      } else {
+        warning(
+          "Providing missing column '",
+          facultatives$Name[y],
+          "' from defaults (",
+          facultatives$Default[y],
+          "). Make sure this is correct.")
+        addOn <- facultatives$Default[y]
       }
-      return(output)
-    },
-    TRUE))){
-    stop("All 'ReactionVolumes' elements in 'x' must be 'numeric' objects of length 2.")
-  }
-  if(!is.numeric(ePC) | length(ePC) != 1){
-    stop("'ePC' must be 'numeric' objects of length 1.")
-  }
-  if(!all(vapply(
-    x,
-    function(y){
-      output <- TRUE
-      if("ePC" %in% names(y)){
-        if(!is.numeric(y$ePC) | length(y$ePC) != 1){
-          output <- FALSE
-        }
-      }
-      return(output)
-    },
-    TRUE))){
-    stop("All 'ePC' elements in 'x' must be 'numeric' objects of length 1.")
-  }
-  if(!is.numeric(ProtC) | length(ProtC) != 1){
-    stop("'ProtC' must be 'numeric' objects of length 1.")
-  }
-  if(!all(vapply(
-    x,
-    function(y){
-      output <- TRUE
-      if("ProtC" %in% names(y)){
-        if(!is.numeric(y$ProtC) | length(y$ProtC) != 1){
-          output <- FALSE
-        }
-      }
-      return(output)
-    },
-    TRUE))){
-    stop("All 'ProtC' elements in 'x' must be 'numeric' objects of length 1.")
-  }
-  if(!all(file.exists(vapply(x,function(y){y$Path},"A")))){
-    stop("All 'Path' elements in 'x' must refer to existing files.")
-  }
-  if(any(file.access(vapply(x,function(y){y$Path},"A"),mode=4) == -1)){
-    stop("All 'Path' elements in 'x' must refer to readable files.")
-  }
-  if(length(AcquisitionTimePoint) != 1){
-    stop("'AcquisitionTimePoint' must be of length 1.")
-  }
-  if(is.na(AcquisitionTimePoint)){
-    warning(c("'AcquisitionTimePoint' not defined. Calculating from spectra. ",
-            "Not appropriate for multiple extract comparisons. You have been ",
-            "warned.See function 'MinimumAcquisitionTime'."))
-  } else {
-    if(!is.numeric(AcquisitionTimePoint)){
-      stop("'AcquisitionTimePoint' must be of class 'numeric'.")
+      tmpX <- cbind(
+        x,
+        rep(x=addOn,times=nrow(x)),
+        stringsAsFactors=FALSE)
+      names(tmpX)[ncol(tmpX)] <- facultatives$Name[y]
+      x <- tmpX
     }
+  }
+  if(!identical(
+    unname(vapply(x[facultatives$Name],class,c(A="A"))),
+    facultatives$Class)){
+    stop(
+      "Facultative columns '",
+      paste(facultatives$Name,collapse="', '"),
+      "' must be of classes '",
+      paste(facultatives$Class,collapse="', '"),
+      "'.")
+  }
+  # Check "Timepoint of Measurement (s)" consistency
+  if(length(unique(x$"Timepoint of Measurement (s)")) != 1){
+    stop("Column 'Timepoint of Measurement (s)' contains multiple values. Exiting.")
   }
   ##############
   # Processing #
@@ -128,25 +132,22 @@ FlippaseDependencyPlot <- function(
   # Parse the data and extract whats needed
   #########################################
   # Parsing
+  #########
   tmpData <- lapply(
-    x,
-    function(y){ParseFluorometerData2(SpecFile=y$Path)})
-  # What intensity to extract?
-  minAT <- vapply(tmpData,function(y){y$"Minimal Acquisition Time (s)"},1)
-  if(!all(minAT[1] == minAT)){
+    x$Path,
+    function(y){ParseFluorometerData2(SpecFile=y)})
+  # What spectral time windows to extract?
+  ########################################
+  minAT <- unique(vapply(tmpData,function(y){y$"Minimal Acquisition Time (s)"},1))
+  if(length(minAT) != 1){
     stop("Minimum acquisition times are not identical - aborting.")
   }
-  minAT <- unique(minAT)
-  tmpAT <- vapply(tmpData,function(y){y$"Maximal Acquisition Time (s)"},1)
-  if(is.na(AcquisitionTimePoint)){
-    maxAT <- min(tmpAT)
+  maxAT <- vapply(tmpData,function(y){y$"Maximal Acquisition Time (s)"},1)
+  if(any(maxAT < x$"Timepoint of Measurement (s)")){
+    stop("'Timepoint of Measurement (s)' is larger than the shortest spectrum 
+         acquisition.")
   } else {
-    if(any(tmpAT < AcquisitionTimePoint)){
-      stop("'AcquisitionTimePoint' is larger than the shortest spectrum 
-           acquisition.")
-    } else {
-      maxAT <- AcquisitionTimePoint
-    }
+    maxAT <- unique(x$"Timepoint of Measurement (s)")
   }
   # Calculations
   ##############
@@ -169,43 +170,16 @@ FlippaseDependencyPlot <- function(
     },
     1)
   # Apply volume correction factors as needed
-  tmpRV <- lapply(
-    tmpData,
-    function(y){
-      if(is.null(y$"Reaction Volumes")){
-        return(ReactionVolumes)
-      } else {
-        return(y$"Reaction Volumes")
-      }
-    })
-  tmpCF <- vapply(tmpRV,function(y){y[2]/y[1]},1)
-  activityIntensity <- activityIntensity * tmpCF
+  correctionFactor <- x$"Reaction Volume Reconstituted (ul)"/x$"Reaction Volume Blank (ul)"
+  activityIntensity <- activityIntensity * correctionFactor
   # Calculate and relativate intensity differences
   deltaIntensity <- 1-(activityIntensity/baselineIntensity)
   # Calculate Protein to egg phosphatidylcholin ratio
-  tmpPC <- vapply(
-    x,
-    function(y){
-      if(is.null(y$"ProtC")){
-        return(ProtC)
-      } else {
-        return(y$"ProtC")
-      }
-    },
-    1)
-  tmpPV <- vapply(x, function(y){return(y$"ProtV")},1)
-  tmpP <- tmpPC * tmpPV
-  tmpEPC <- vapply(
-    x,
-    function(y){
-      if(is.null(y$"ePC")){
-        return(ePC)
-      } else {
-        return(y$"ePC")
-      }
-    },
-    FUN.VALUE=1)
-  tmpPPC <- tmpP/tmpEPC
+  proteinAmount <- x$"Extract Protein Concentration (mg/ml)" * x$"Extract Volume (ul)"
+  proteinToEpcRatio <- proteinAmount/x$"Concentration Egg PC (mM)"
+  # Fit a monoexponential curve to the data
+  #########################################
+  
   ###################
   # Assemble output #
   ###################
