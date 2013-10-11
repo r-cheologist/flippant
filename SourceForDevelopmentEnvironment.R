97a5da17b49890a0594097d0ac0e0f8db76cb2db
Johannes Graumann <jog2030@qatar-med.cornell.edu>
2013-10-11 20:47:29 +0300

    Ensure that dev_mode switching only happens when we are not already in that mode;

--- SourceForDevelopmentEnvironment.R
+++ SourceForDevelopmentEnvironment.R
@@ -1,8 +1,12 @@
 # Load the required libraries
 library(devtools)
-library(testthat)
 # Switch to developer's mode
-dev_mode(path="~/R-DevTools/mdaa-Development")
+devPath <- path.expand("~/R-DevTools/mdaa-Development")
+if(!(devPath %in% .libPaths())){
+  dev_mode(path=devPath)
+  update.packages(checkBuilt=TRUE)
+}
+library(testthat)
 # Ensure correct RCFPD package
 library(RCFPD,lib.loc="~/R-DevTools/mdaa-Development")
 # Load the worked on package
