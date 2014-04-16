421bfe2db5dd97471905dce7dec5f3e654ba375a
Johannes Graumann <jog2030@qatar-med.cornell.edu>
2014-04-16 17:09:29 +0300

    Purge docuemtnation and reference documented "dithioniteFlippaseAssayPlot"; Rename function; Adapt to renaming of helper functions;

--- R/dithionite_flippase_assay_traces.R
+++ R/dithionite_flippase_assay_traces.R
@@ -1,68 +1,14 @@
-#' @title dithionite_flippase_assay_traces
-#' @description A function displaying raw traces for dithionite flippase assays
-#' @details Input format for and data processing by the function is documented 
-#' at \code{\link{dithionite_flippase_assay_plot}}.
-#' @param x \code{\link{data.frame}} as described in 
-#' \code{\link{dithionite_flippase_assay_plot}}.
-#' @param scale_to Defines the source of \code{ymax}, defaulting to 
-#' \code{model}. See \code{\link{dithionite_flippase_assay_plot}}.
-#' @return Returns a \code{\link{ggplot}} object.
-#' @author Johannes Graumann
+#' @rdname dithioniteFlippaseAssayPlot
 #' @export
-#' @seealso \code{\link{parse_fluorometer_output}},
-#' \code{\link{dithionite_flippase_assay_plot}},
-#' \code{\link{dithionite_flippase_assay_input_validation}},
-#' \code{\link{dithionite_flippase_assay_calculations}}
-#' \code{\link{dithionite_flippase_assay_stats}},
-#' @keywords methods manip
-#' @examples
-#' stop("Add citation to Mike's manuscript!")
-#' stop("Revisit workflow description.")
-#' stop("Add example using actually published data.")
-#' # Build input
-#' x <- data.frame(
-#'  Path = c(
-#'  "~/localTmp/Fluor Data_Menon Lab//21FEB2013_Erg1 immun_deple/ePC.txt",
-#'  "~/localTmp/Fluor Data_Menon Lab/21FEB2013_Erg1 immun_deple/Erg1 TE-minus-15ul.txt",
-#'  "~/localTmp/Fluor Data_Menon Lab/21FEB2013_Erg1 immun_deple/Erg1 TE-minus-40ul.txt",
-#'  "~/localTmp/Fluor Data_Menon Lab/21FEB2013_Erg1 immun_deple/Erg1 TE-minus-75ul.txt",
-#'  "~/localTmp/Fluor Data_Menon Lab/21FEB2013_Erg1 immun_deple/Erg1 TE-minus-150ul.txt",
-#'  "~/localTmp/Fluor Data_Menon Lab//21FEB2013_Erg1 immun_deple/ePC.txt",
-#'  "~/localTmp/Fluor Data_Menon Lab/21FEB2013_Erg1 immun_deple/Erg1 TE-plus-15ul.txt",
-#'  "~/localTmp/Fluor Data_Menon Lab//21FEB2013_Erg1 immun_deple/Erg1 TE-plus-40ul.txt",
-#'  "~/localTmp/Fluor Data_Menon Lab//21FEB2013_Erg1 immun_deple/Erg1 TE-plus-75ul.txt",
-#'  "~/localTmp/Fluor Data_Menon Lab//21FEB2013_Erg1 immun_deple/Erg1 TE-plus-150ul.txt",
-#'  "~/localTmp/Fluor Data_Menon Lab//21FEB2013_Erg1 immun_deple/ePC.txt",
-#'  "~/localTmp/Fluor Data_Menon Lab/21FEB2013_Erg1 immun_deple/Erg1 TE-minus-15ul.txt",
-#'  "~/localTmp/Fluor Data_Menon Lab/21FEB2013_Erg1 immun_deple/Erg1 TE-minus-40ul.txt",
-#'  "~/localTmp/Fluor Data_Menon Lab/21FEB2013_Erg1 immun_deple/Erg1 TE-minus-75ul.txt",
-#'  "~/localTmp/Fluor Data_Menon Lab/21FEB2013_Erg1 immun_deple/Erg1 TE-minus-150ul.txt",
-#'  "~/localTmp/Fluor Data_Menon Lab//21FEB2013_Erg1 immun_deple/ePC.txt",
-#'  "~/localTmp/Fluor Data_Menon Lab/21FEB2013_Erg1 immun_deple/Erg1 TE-plus-15ul.txt",
-#'  "~/localTmp/Fluor Data_Menon Lab//21FEB2013_Erg1 immun_deple/Erg1 TE-plus-40ul.txt",
-#'  "~/localTmp/Fluor Data_Menon Lab//21FEB2013_Erg1 immun_deple/Erg1 TE-plus-75ul.txt",
-#'  "~/localTmp/Fluor Data_Menon Lab//21FEB2013_Erg1 immun_deple/Erg1 TE-plus-150ul.txt"),
-#'  "Extract Volume (ul)" = c(0,15,40,75,150,0,15,40,75,150,0,15,40,75,150,0,15,40,75,150),
-#'  #     "Reaction Volume w/o DT (ul)" = rep(2000,4),
-#'  "Reaction Volume with DT (ul)" = rep(2040,20),
-#'  "Concentration Egg PC (mM)" = rep(4.5,20),
-#'  "Extract Protein Concentration (mg/ml)" = c(rep(0.67,5),rep(1.26,5),rep(0.67,5),rep(1.26,5)),
-#'  #     "Timepoint of Measurement (s)",
-#'  "Experimental Series"=c(rep("Extract",5),rep("Depleted Extract",5),rep("Extract",5),rep("Depleted Extract",5)),
-#'  Experiment=c(rep("Erg1, Replicate 1",10),rep("Erg1, Replicate 2",10)),
-#'  check.names=FALSE,
-#'  stringsAsFactors=FALSE)
-#'  # Run function
-#'  DithioniteFlippaseAssay(x)
-dithionite_flippase_assay_traces <- function(x,scale_to=c("model","data")){
+dithioniteFlippaseAssayTraces <- function(x,scale_to=c("model","data")){
 # Check Prerequisites -----------------------------------------------------
-  validated_params <- dithionite_flippase_assay_input_validation(x=x,scale_to=scale_to)
+  validated_params <- dithioniteFlippaseAssayInputValidation(x=x,scale_to=scale_to)
   x <- validated_params[["x"]]
   scale_to <- validated_params[["scale_to"]]
 
 # Processing --------------------------------------------------------------
   # Perform assay calculations to retrive PPR
-  processed_list_from_x <- dithionite_flippase_assay_calculations(x=x,scale_to=scale_to)
+  processed_list_from_x <- dithioniteFlippaseAssayCalculations(x=x,scale_to=scale_to)
   trimmed_processed_list_from_x <- rbind.fill(
       lapply(
         names(processed_list_from_x),
