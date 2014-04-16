2adf36a4102a3080a18e2e99c8a7262f589e422e
Johannes Graumann <jog2030@qatar-med.cornell.edu>
2014-04-16 17:12:10 +0300

    Properly announce use of assertive;

--- R/onAttach.R
+++ R/onAttach.R
@@ -15,6 +15,7 @@
   vC <- checkVersion(
     expectedR=package_version("3.1.0"),
     expectedPackages=list(
+      assertive=package_version("0.1-8"),
       ggplot2=package_version("0.9.3.1"),
       pdapbase=package_version("0.0-1"),
       plyr=package_version("1.8.1"),
