d504a44b5ec76dc3cc5d731c4ffc2abe153eae59
Johannes Graumann <jog2030@qatar-med.cornell.edu>
2014-05-12 20:45:28 +0300

    Adapt to current development environment;

--- R/onAttach.R
+++ R/onAttach.R
@@ -19,8 +19,8 @@
       ggplot2=package_version("0.9.3.1"),
       pdapbase=package_version("0.0-1"),
       plyr=package_version("1.8.1"),
-      robustbase=package_version("0.90-2"),
-      scales=package_version("0.2.3")))
+      robustbase=package_version("0.91-1"),
+      scales=package_version("0.2.4")))
   if(!is.null(vC)){
     packageStartupMessage(vC)
   }
