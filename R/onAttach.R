fcd6b2c211b720310128c64c16b63a91b25b1e8f
Johannes Graumann <jog2030@qatar-med.cornell.edu>
2014-07-08 15:09:00 -0400

    Fix imports for dithionite addition determination;

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,3 +1,4 @@
+#' @importFrom pdapbase checkVersion
 .onAttach <- function(...){
   vC <- checkVersion(
     expectedR=package_version("3.1.0"),
@@ -6,8 +7,10 @@
       ggplot2=package_version("1.0.0"),
       pdapbase=package_version("0.0-1"),
       plyr=package_version("1.8.1"),
+      RcppRoll=package_version("0.1.0"),
       robustbase=package_version("0.91-1"),
-      scales=package_version("0.2.4")))
+      scales=package_version("0.2.4"),
+      wmtsa=package_version("2.0-0")))
   if(!is.null(vC)){
     packageStartupMessage(vC)
   }
