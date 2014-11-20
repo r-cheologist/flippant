97279013a6307e111882e640118fada69be097b0
Johannes Graumann <jog2030@qatar-med.cornell.edu>
2014-11-20 11:21:06 -0500

    Adapt to new pdapbase version and increment version number;

--- R/onAttach.R
+++ R/onAttach.R
@@ -6,7 +6,7 @@
       assertive = package_version("0.1-8"),
       ggplot2 = package_version("1.0.0"),
       nlmrt = package_version("2013-9.24"),
-      pdapbase = package_version("0.0-1"),
+      pdapbase = package_version("0.0-4"),
       plyr = package_version("1.8.1"),
       RcppRoll = package_version("0.1.0"),
       splus2R = package_version("1.2-0"),
