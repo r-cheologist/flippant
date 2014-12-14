deeaa607ea9ea24437485241bf6489f1b119bdd2
Johannes Graumann <jog2030@qatar-med.cornell.edu>
2014-12-14 08:16:14 +0300

    Adapt to current development environment (nlmrt updated);

--- R/onAttach.R
+++ R/onAttach.R
@@ -5,7 +5,7 @@
     expectedPackages = list(
       assertive = package_version("0.1-8"),
       ggplot2 = package_version("1.0.0"),
-      nlmrt = package_version("2013-9.24"),
+      nlmrt = package_version("2013-9.25"),
       pdapbase = package_version("0.0-4"),
       plyr = package_version("1.8.1"),
       RcppRoll = package_version("0.1.0"),
