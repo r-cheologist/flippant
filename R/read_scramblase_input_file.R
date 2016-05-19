ceb7ce17fcf0f831d40129e7d7215e6a04b4f52f
Johannes Graumann <jog2030@qatar-med.cornell.edu>
2016-05-18 21:46:03 -0400

    First minor start of magrittrization;

--- R/read_scramblase_input_file.R
+++ R/read_scramblase_input_file.R
@@ -1,17 +1,18 @@
 #' @importFrom assertive assert_all_are_readable_files
 #' @importFrom assertive assert_is_a_non_empty_string
+#' @importFrom magrittr %>%
 #' @importFrom utils read.delim
 read_scramblase_input_file <- function(x){
-  assert_is_a_non_empty_string(x)
-  assert_all_are_readable_files(x)
-  output <- read.delim(
-    file=x,
-    header=TRUE,
-    sep="\t",
-    quote="\"'",
-    fill=TRUE,
-    comment.char="#",
-    stringsAsFactors=FALSE,
-    check.names=FALSE)
-  return(output)
+  x %>%
+    assert_is_a_non_empty_string() %>%
+    assert_all_are_readable_files() %>%
+    read.delim(
+      header=TRUE,
+      sep="\t",
+      quote="\"'",
+      fill=TRUE,
+      comment.char="#",
+      stringsAsFactors=FALSE,
+      check.names=FALSE) %>%
+    return()
 }
