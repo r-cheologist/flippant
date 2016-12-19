00f66b83fc864a04278b5e6da8c09da1a21fc5c4
Johannes Graumann <jog2030@qatar-med.cornell.edu>
2016-12-19 14:50:41 +0100

    Adapt assay citations as proposed by CRAN team;

--- R/scramblase_assay_plot.R
+++ R/scramblase_assay_plot.R
@@ -167,18 +167,16 @@
 #' assay statistics as a \code{\link{data.frame}}.
 #' @author Johannes Graumann
 #' @references Menon, I., Huber, T., Sanyal, S., Banerjee, S., Barre, P., Canis, 
-#' S., Warren, J.D., Hwa, J., Sakmar, T.P., and Menon, A.K. (2011). Opsin Is a 
-#' Phospholipid Flippase. Current Biology 21, 149-153.
+#' S., Warren, J.D., Hwa, J., Sakmar, T.P., and Menon, A.K. (2011)
+#' <DOI:10.1016/j.cub.2010.12.031>
 #' 
 #' Goren, M.A., Morizumi, T., Menon, I., Joseph, J.S., Dittman, J.S., 
-#' Cherezov, V., Stevens, R.C., Ernst, O.P., and Menon, A.K. (2014). 
-#' Constitutive phospholipid scramblase activity of a G Protein-coupled 
-#' receptor. Nat Commun 5, 5115.
+#' Cherezov, V., Stevens, R.C., Ernst, O.P., and Menon, A.K. (2014)
+#' <DOI:10.1038/ncomms6115>
 #' 
 #' Ploier, B., Caro, L.N., Morizumi, T., Pandey, K., Pearring, J.N.,
 #' Goren, M.A., Finnemann, S.C., Graumann, J., Arshavsky, V.Y., Dittman, J.S.,
-#' Ernst, O.P., Menon, A.K. (2016). Dimerization deficiency of enigmatic
-#' retinitis pigmentosa-linked rhodopsin mutants. Nat Commun. In Press.
+#' Ernst, O.P., Menon, A.K. (2016). <DOI:10.1038/ncomms12832>
 #' @export
 #' @seealso \code{\link{parse_fluorimeter_output}} \code{\link{nlsLM}}
 #' @import ggplot2
