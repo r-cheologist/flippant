0fcc6203fa181428339d4d93437678bfae60af23
Johannes Graumann <jog2030@qatar-med.cornell.edu>
2019-05-15 23:53:40 +0200

    Code prettifications & annotation of traces with relative fluorescence change implemented in `scramblase_assay_traces`;

--- R/scramblase_assay_traces.R
+++ R/scramblase_assay_traces.R
@@ -3,21 +3,24 @@
 scramblase_assay_traces <- function(
   x,
   ppr_scale_factor = 0.65,
-  time_min_sec=NA_real_,
-  time_max_sec=NA_real_,
-  adjust = TRUE){
+  time_min_sec = NA_real_,
+  time_max_sec = NA_real_,
+  adjust = TRUE,
+  timepoint_of_measurement = 400,
+  n_averaging = 10,
+  annotate_traces = FALSE) {
   UseMethod("scramblase_assay_traces", x)
 }
 #' @export
 scramblase_assay_traces.data.frame <- function(
   x,
-  ...){
+  ...) {
   base_function_scramblase_assay_traces(x, ...)
 }
 
 #' @export
-scramblase_assay_traces.character <- function(x, ...){
-  parsedInputFile <- read_scramblase_input_file(x)
+scramblase_assay_traces.character <- function(x, ...) {
+  parsedInputFile <- flippant:::read_scramblase_input_file(x)
   withr::with_dir(
     dirname(x),
     base_function_scramblase_assay_traces(x = parsedInputFile, ...)
@@ -26,12 +29,16 @@ scramblase_assay_traces.character <- function(x, ...){
 base_function_scramblase_assay_traces <- function(
   x,
   ppr_scale_factor = 0.65,
-  time_min_sec=NA_real_,
-  time_max_sec=NA_real_,
-  adjust = TRUE){
+  time_min_sec = NA_real_,
+  time_max_sec = NA_real_,
+  adjust = TRUE,
+  timepoint_of_measurement = 400,
+  n_averaging = 10,
+  annotate_traces = FALSE) {
 # Check Prerequisites -----------------------------------------------------
-  protein_is_factor <- is.character(x$`Protein Reconstituted (mg)`)
-  validatedParams <- scramblase_assay_input_validation(
+  protein_is_factor = class(x$`Protein Reconstituted (mg)`) %in%
+    c("character", "factor")
+  validatedParams <- flippant:::scramblase_assay_input_validation(
     x = x,
     scale_to = "data",
     ppr_scale_factor = ppr_scale_factor,
@@ -46,6 +53,11 @@ base_function_scramblase_assay_traces <- function(
   x <- validatedParams[["x"]]
   assertive.types::assert_is_a_number(time_min_sec)
   assertive.types::assert_is_a_number(time_max_sec)
+  assertive.types::assert_is_a_number(timepoint_of_measurement)
+  assertive.types::assert_is_a_number(n_averaging)
+  assertive.numbers::assert_all_are_whole_numbers(n_averaging)
+  assertive.numbers::assert_all_are_positive(n_averaging)
+  assertive.types::assert_is_a_bool(annotate_traces)
 
 # Processing --------------------------------------------------------------
   columns_retained <- c("Path", "Experimental Series", "Experiment")
@@ -62,24 +74,44 @@ base_function_scramblase_assay_traces <- function(
   }
   processedX <- processedX %>% magrittr::extract(columns_retained)
   # Parse the fluorimeter data and whip it into shape
-  rawFluorimeterOutput <- lapply(processedX$Path,parse_fluorimeter_output,adjust = adjust)
+  rawFluorimeterOutput <- lapply(
+    processedX$Path, parse_fluorimeter_output,
+      adjust = adjust, timepoint_of_measurement = timepoint_of_measurement,
+      n_averaging = n_averaging)
   names(rawFluorimeterOutput) <- processedX$Path
-  dataFromRawFluorimeterOutput <- lapply(
+  dataFromRawFluorimeterOutput <- plyr::rbind.fill(
+    lapply(
       names(rawFluorimeterOutput),
-      function(y){
+      function(y) {
         time.in.sec <- rawFluorimeterOutput[[y]][["Time.in.sec"]]
         fluorescenceIntensity <- rawFluorimeterOutput[[y]][["Fluorescence.Intensity"]]
-        fluorescenceIntensity <- fluorescenceIntensity/max(fluorescenceIntensity,na.rm = TRUE)
+        fluorescenceIntensity <- fluorescenceIntensity/max(fluorescenceIntensity, na.rm = TRUE)
         return(
           data.frame(
             Path = y,
             Time.in.sec = time.in.sec,
             Fluorescence.Intensity = fluorescenceIntensity))
-      }) %>%
-    plyr::rbind.fill()
+    }))
+  # Deal with fluorescence extrema
+  fluorescence_extrema <- rawFluorimeterOutput %>%
+    names() %>%
+    lapply(function(x) {
+      attr(rawFluorimeterOutput[[x]], "FluorescenceExtrema") %>%
+        magrittr::divide_by(
+          rawFluorimeterOutput[[x]][["Fluorescence.Intensity"]] %>%
+            max(na.rm = TRUE)) %>%
+        as.list() %>% c(list(Path = x))}) %>%
+    lapply(as.data.frame, stringsAsFactors = FALSE) %>%
+    dplyr::bind_rows() %>%
+    dplyr::mutate(
+      Fractional.Fluorescence.Change = Endpoint.Fluorescence %>%
+        magrittr::divide_by(Baseline.Fluorescence),
+      )
   # Merge spectral data and analysis
   mergedData <- merge(x = dataFromRawFluorimeterOutput, y = processedX,
                       by = "Path")
+  mergedData %<>% merge(y = fluorescence_extrema, by = "Path") %>%
+    dplyr::mutate(Path = as.character(Path))
   names(mergedData) <- make.names(names(mergedData))
   if (!protein_is_factor) {
     # Use corresponding PPR as a trace identifier
@@ -102,16 +134,13 @@ base_function_scramblase_assay_traces <- function(
   # Restrict x axis as requested
   if (any(!is.na(c(time_min_sec,time_max_sec)))) {
     xRange <- range(mergedData$Time.in.sec, na.rm = TRUE)
-    if (!is.na(time_min_sec)) xRange[1] <- time_min_sec
-    if (!is.na(time_max_sec)) xRange[2] <- time_max_sec
+    if (!is.na(time_min_sec)) {xRange[1] <- time_min_sec}
+    if (!is.na(time_max_sec)) {xRange[2] <- time_max_sec}
     plotOutput <- plotOutput + xlim(xRange)
   }
   # Render color scale friendly to the color blind
-  plotOutput <- if (protein_is_factor) {
-    plotOutput + scale_color_brewer(palette = "YlGnBu")
-  } else {
-    plotOutput + scale_color_continuous(low = "#0072B2",high = "#E69F00")
-  }
+  # plotOutput <- plotOutput + 
+  #   scale_color_continuous(low = "#0072B2",high = "#E69F00")
   # Prettify
   plotOutput <- plotOutput +
     labs(
@@ -130,17 +159,29 @@ base_function_scramblase_assay_traces <- function(
           colour = expression("adj. PPR "*bgroup("(",frac("mg","mmol"),")")))
     }
   }
+  # Annotate as requested
+  if (annotate_traces) {
+    plotOutput <- plotOutput +
+      ggplot2::geom_text(
+        data = dplyr::distinct(plotOutput$data, Path, .keep_all = TRUE), 
+        mapping = ggplot2::aes(
+          label = Fractional.Fluorescence.Change %>%
+            magrittr::multiply_by(100) %>%
+            round() %>%
+            paste0("%"),
+          x = Inf, y = Endpoint.Fluorescence),
+        position = ggplot2::position_nudge(y = 0.05), vjust = 0, hjust = 1,
+        color = "black", size = 3)
+  }
   # Facetting
   hasExperiment <- any(!is.na(mergedData$Experiment))
   hasSeries <- any(!is.na(mergedData$Experimental.Series))
-  plotOutput <- if (hasExperiment && hasSeries) {
-    plotOutput + facet_grid(Experimental.Series~Experiment)
+  if (hasExperiment && hasSeries) {
+    plotOutput <- plotOutput + facet_grid(Experimental.Series~Experiment)
   } else if (hasExperiment) {
-    plotOutput + facet_wrap(~Experiment)
+    plotOutput <- plotOutput + facet_wrap(~Experiment)
   } else if (hasSeries) {
-    plotOutput + facet_wrap(~Experimental.Series)
-  } else {
-    plotOutput
+    plotOutput <- plotOutput + facet_wrap(~Experimental.Series)
   }
   return(plotOutput)
 }
