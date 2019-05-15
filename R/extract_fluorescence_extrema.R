0c99762214de48d9546ce6ee92ed516717b23ac2
Johannes Graumann <jog2030@qatar-med.cornell.edu>
2019-05-15 23:50:23 +0200

    More precise error reporting in `extract_fluorescence_extrema` - now also including the `timepoint_of_measurement` used when failing into message;

--- R/extract_fluorescence_extrema.R
+++ R/extract_fluorescence_extrema.R
@@ -24,7 +24,8 @@ extract_fluorescence_extrema <- function(
   if (nrow(min_fluorescence_to_extract) < n_averaging) {
     stop("Insufficient baseline data points (n = ",
          nrow(min_fluorescence_to_extract),
-         "to extract where 'n_averaging' equals ", n_averaging, ".")
+         "to extract where 'n_averaging' equals '", n_averaging,
+         "' and 'timepoint_of_measurment' is '", timepoint_of_measurement, "'.")
   }
 
   # Extract endpoint fluorescence -------------------------------------------
@@ -34,7 +35,8 @@ extract_fluorescence_extrema <- function(
   if (nrow(max_fluorescence_to_extract) < n_averaging) {
     stop("Insufficient endpoint data points (n = ",
          nrow(max_fluorescence_to_extract),
-         "to extract where 'n_averaging' equals ", n_averaging, ".")
+         "to extract where 'n_averaging' equals '", n_averaging,
+         "' and 'timepoint_of_measurment' is '", timepoint_of_measurement, "'.")
   }
 
   # Averaging ---------------------------------------------------------------
